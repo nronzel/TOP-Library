e05e772b5cef8441b305e855f3b2c06b2c64b23d
nronzel <86695181+nronzel@users.noreply.github.com>
2022-11-23 10:57:28 -0500

    set up files and started basic style

---
+++ script.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    self.title = title;
+    self.author = author;
+    self.pages = pages;
+    self.read = read;
+}
+
+function addBook() {
+    
+}
