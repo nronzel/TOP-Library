4a0590bcbfa4cfdc764b1df4de47171d171b0f3f
nronzel <86695181+nronzel@users.noreply.github.com>
2022-11-23 14:35:05 -0500

    added logic for the 'mark read' buttons

--- script.js
+++ script.js
@@ -8,5 +8,24 @@ function Book(title, author, pages, read) {
 }
 
 function addBook() {
-    
+
 }
+
+
+
+const readBtn = document.querySelectorAll('#read');
+// changes button between READ and NOT READ states
+readBtn.forEach((button) => {
+    button.addEventListener('click', () => {
+        const arr = Array.from(button.classList)
+        if (arr.includes('mark-read')) {
+            button.classList.add('not-read');
+            button.classList.remove('mark-read')
+            button.textContent = "NOT READ";
+        } else if (arr.includes('not-read')) {
+            button.classList.add('mark-read');
+            button.classList.remove('not-read')
+            button.textContent = "READ";
+        }
+    })
+})
