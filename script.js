81afe9e466481805b1ac1070f909fc0e75b06d89
nronzel <86695181+nronzel@users.noreply.github.com>
2022-11-24 10:45:29 -0500

    implemented modal

--- script.js
+++ script.js
@@ -7,43 +7,45 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-function addBook() {
-    const newCard = document
-        .createElement('div')
-        .classList("card");
-    console.log(newCard);
+// function addBook() {
+//     const newCard = document.createElement('div');
+//     const 
+//     console.log(newCard);
+// }
+
+function markRead() {
+    const readBtn = document.querySelectorAll('#read-btn');
+    // changes button between READ and NOT READ states
+    readBtn.forEach((button) => {
+        button.addEventListener('click', () => {
+            const arr = Array.from(button.classList)
+            if (arr.includes('mark-read')) {
+                button.classList.add('not-read');
+                button.classList.remove('mark-read')
+                button.textContent = "NOT READ";
+            } else if (arr.includes('not-read')) {
+                button.classList.add('mark-read');
+                button.classList.remove('not-read')
+                button.textContent = "READ";
+            }
+        });
+    });
 }
-
-const readBtn = document.querySelectorAll('#read-btn');
-// changes button between READ and NOT READ states
-readBtn.forEach((button) => {
-    button.addEventListener('click', () => {
-        const arr = Array.from(button.classList)
-        if (arr.includes('mark-read')) {
-            button.classList.add('not-read');
-            button.classList.remove('mark-read')
-            button.textContent = "NOT READ";
-        } else if (arr.includes('not-read')) {
-            button.classList.add('mark-read');
-            button.classList.remove('not-read')
-            button.textContent = "READ";
-        }
-    })
-})
-
+markRead();
+const modalCont = document.querySelector(".modal-container");
+const btnOpen = document.querySelector(".btn-open");
+const btnClose = document.querySelector(".btn-close");
+const overlay = document.querySelector(".overlay");
 // button to prompt for input
-function buttonListen() {
-    const formBtn = document.querySelector(".btn-open");
-    formBtn.addEventListener('click', () => {
-        formPrompt();
-    })
+function openModal() {
+    modalCont.classList.add('active');
+    overlay.classList.add('active');
 }
 
-function formPrompt() {
-    let title = prompt("Book Title: ");
-    let author = prompt("Author: ");
-    let pages = prompt("Pages: ");
-    const newBook = new Book(title, author, pages, false);
-    myLibrary.unshift(newBook);
+function closeModal() {
+    modalCont.classList.remove('active');
+    overlay.classList.remove('active');
 }
-
+btnOpen.onclick = openModal;
+overlay.onclick = closeModal;
+btnClose.onclick = closeModal;
