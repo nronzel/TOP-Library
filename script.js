df74b9db4613133903ce2536e3654b15a9cd9851
nronzel <86695181+nronzel@users.noreply.github.com>
2022-11-25 10:49:59 -0500

    added functionality to add new card to library

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
+// constructor function to create book objects
+
+function Book(title="undefined", author="undefined", pages="0", read=false) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
 
-const bookContainer = document.querySelector('.book-container')
+// global selectors
 
-function addBook(book) {
+const bookContainer = document.querySelector('.book-container')
+const modalCont = document.querySelector(".modal-container");
+const btnOpen = document.querySelector(".btn-open");
+const btnClose = document.querySelector(".btn-close");
+const overlay = document.querySelector(".overlay");
+const modalSubmit = document.querySelector(".modal-submit")
+
+// creates a new card
+
+const newBookCard = (book) => {
+
+    // selectors
+
     const newCard = document.createElement('div');
     const title = document.createElement('p');
     const author = document.createElement('p')
@@ -17,15 +31,47 @@ function addBook(book) {
     const buttonGroup = document.createElement('div')
     const markRead = document.createElement('button')
     const remove = document.createElement('button')
+    
+
+    // adds classes
 
     newCard.classList.add('card')
     buttonGroup.classList.add('card-buttons')
     markRead.classList.add('button-style')
-    remove.classList.add('button-style remove')
-}
+    markRead.setAttribute('id', 'read-btn')
+    remove.classList.add('remove', 'button-style')
 
-// changes button between READ and NOT READ states
-function markRead() {
+    const readYet = document.getElementById('readYet').checked
+
+    if (readYet === true) {
+        markRead.classList.add('mark-read')
+        markRead.textContent = 'READ';
+        markRead.setAttribute('value', 'read')
+    } else {
+        markRead.classList.add('not-read')
+        markRead.textContent = 'NOT READ'
+        markRead.setAttribute('value', 'not-read')
+    }
+
+    console.log(markRead.value);
+
+    // adds text content
+
+    title.textContent = `"${book.title}"`
+    author.textContent = `${book.author}`
+    pages.textContent = `${book.pages} pages`
+    remove.textContent = "Remove"
+
+    // appends each element to the respective parent node
+
+    newCard.appendChild(title)
+    newCard.appendChild(author)
+    newCard.appendChild(pages)
+    buttonGroup.appendChild(markRead)
+    buttonGroup.appendChild(remove)
+    newCard.appendChild(buttonGroup)
+    bookContainer.appendChild(newCard)
+
     const readBtn = document.querySelectorAll('#read-btn');
     readBtn.forEach((button) => {
         button.addEventListener('click', () => {
@@ -41,22 +87,63 @@ function markRead() {
             }
         });
     });
-}
-markRead();
-const modalCont = document.querySelector(".modal-container");
-const btnOpen = document.querySelector(".btn-open");
-const btnClose = document.querySelector(".btn-close");
-const overlay = document.querySelector(".overlay");
 
-function openModal() {
+    
+}
+
+modalSubmit.addEventListener('click', (e) => {
+    e.preventDefault();
+    const newBook = getBookInput()
+    myLibrary.push(newBook);
+    console.log(myLibrary);
+    updateDisplay(newBook);
+    closeModal();
+})
+
+
+
+const getBookInput = () => {
+    const title = document.getElementById('title').value
+    const author = document.getElementById('author').value
+    const pages = document.getElementById('pages').value
+    const read = document.getElementById('readYet').checked
+    return new Book(title, author, pages, read)
+}
+
+
+
+const updateDisplay = (book) => {
+    newBookCard(book);
+}
+
+
+
+
+
+
+
+
+
+// changes button between READ and NOT READ states
+
+
+
+
+
+// modal functionality 
+
+const openModal = () => {
+    bookForm.reset();
     modalCont.classList.add('active');
     overlay.classList.add('active');
 }
 
-function closeModal() {
+const closeModal = () => {
     modalCont.classList.remove('active');
     overlay.classList.remove('active');
 }
+
 btnOpen.onclick = openModal;
 overlay.onclick = closeModal;
 btnClose.onclick = closeModal;
+// modalSubmit.onclick = addBook;
